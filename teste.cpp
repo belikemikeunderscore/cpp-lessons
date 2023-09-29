a9d90c3bb336e2647275fdbe4691e1ec3f4fa447
Miguel Brás <miguel.bras06@gmail.com>
2023-09-29 10:34:49 +0000

    não sei honestamente

--- teste.cpp
+++ teste.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 using namespace std;
 
+int NUMPORT = 5; //caso fosse tipo um numero de uma porta oualgo assim do genero 
+
+
+/*
+* comentarios multilinha
+*
+*
+*
+*/
+
+
 int main()
 {
-    cout << "Hello World!\n";
+    cout << "Hello \\ World!\n";
     cout << "I'm a C++ program\n";
+    cout << "And I love women.";
     return 0;
 }
 
+void teste(int a){
+
+
+}
