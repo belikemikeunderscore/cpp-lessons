68c68b5960e9ffb4784034c4c2269906ff2d1930
Miguel Brás <miguel.bras06@gmail.com>
2023-09-29 10:26:35 +0000

    cpp coisas coisas

--- teste.cpp
+++ teste.cpp
@@ -1,2 +1,10 @@
-#include <stdio.h>
-#include <teste2.cpp>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "I'm a C++ program\n";
+    return 0;
+}
+
