c3e261ae291d74a106b65b518733753b8bf8cf77
Miguel Brás <miguel.bras06@gmail.com>
2023-10-06 09:45:33 +0000

    mais coisinhas

--- teste.cpp
+++ teste.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int NUMPORT = 5; //caso fosse tipo um numero de uma porta oualgo assim do genero 
+int numero_int = 1; //numeros inteiros
+double numero_real = 3.5; //numeros reais
+char caracter = 'c'; //caracteres
+string textinho = "textinho !!!"; //textinho !!!!
 
 
 /*
 * comentarios multilinha
-*
+* yay !
 *
 *
 */
-
 
 int main()
 {
     cout << "Hello \\ World!\n";
     cout << "I'm a C++ program\n";
-    cout << "And I love women.";
+    cout << "And I love women.\n";
+    cout << "de momento, temos" + numero_int;
     return 0;
 }
 
