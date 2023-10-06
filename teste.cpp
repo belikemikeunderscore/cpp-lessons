4825b36977657d1ab0ccce1882b5279dd51ce5df
Miguel Brás <miguel.bras06@gmail.com>
2023-10-06 10:29:55 +0000

    read me

--- teste.cpp
+++ teste.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int numero_int = 0; //numeros inteiros
-double numero_real = 3.5; //numeros reais
-char caracter = 'c'; //caracteres
+//variaveis
+int numero_int = 0;                     //numeros inteiros
+double numero_real = 3.5;               //numeros reais
+char caracter = 'c';                    //caracteres
 string textinho = "de momento, temos "; //textinho !!!!
-
-
-/*
-* comentarios multilinha
-* yay !
-*
-*
-*/
+bool booleane = true;                   //boolianos bolinhos 
+int numeroDeBitches;
 
 int main()
 {
-    cout << "Hello \\ World!\n";
+    cout << "Hello World!\n";
     cout << "I'm a C++ program\n";
     cout << "And I love women.\n";
-    cout << textinho; 
-    cout << numero_int;
-    cout << " bitches";
+    cout << "Indique o numero de bitches atual";
+    cin >> numeroDeBitches;
+    cout << "de momento, temos " << numero_int << " bitches"; 
     return 0;
 }
 
